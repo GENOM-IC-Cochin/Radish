37d9b77bc57e8c76e17df7455e74ec27c6d441f0
Paul Etheimer <paul.etheimer@inserm.fr>
2022-06-30 16:15:50 +0200

    added rprojroot to find project root

--- inst/app/ui.R
+++ inst/app/ui.R
@@ -1,2 +1,4 @@
-if (isTRUE(getOption("shiny.testmode"))) pkgload::load_all("../../")
+if (isTRUE(getOption("shiny.testmode"))) {
+  pkgload::load_all(rprojroot::is_r_package$find_file())#"../../")
+}
 ui <- Radish::radish_ui()
