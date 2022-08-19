767843756556c678f9f1c35e353d0620a91ba48c
Paul Etheimer <paul.etheimer@inserm.fr>
2022-08-19 13:33:52 +0200

    Fixed gha R version + disable skip on cran
    NOT_CRAN is not set by shinytest2::test_app()
    So testing this way skips everything

--- tests/testthat/test-radish_app.R
+++ tests/testthat/test-radish_app.R
@@ -3,7 +3,7 @@ library(shinytest2)
 
 test_that("Radish works", {
   # Don't run these tests on the CRAN build servers
-  skip_on_cran()
+  ## skip_on_cran()
 
   appdir <- system.file(package = "Radish", "app")
   test_app(appdir)
